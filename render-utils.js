8d00d6eb6fe99ba6c8770e2f62a99941b0999f27
amanda-short <AmandaShort@Kamies-MacBook-Pro.local>
2022-10-11 17:02:57 -0700

    render creation

---
+++ render-utils.js
@@ -0,0 +1,8 @@
+export function renderPost(post) {
+    const li = document.createElement('li');
+    li.classList.add('post');
+
+    const div = document.createElement('div');
+    div.classList.add('user');
+    div.textContent = 
+}
